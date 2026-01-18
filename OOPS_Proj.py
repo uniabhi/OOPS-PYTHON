590faaf0e14ebb3dc3adc8db913dbba7e1be0a9b
uniabhi <uniabhi5684@gmail.com>
2026-01-18 13:05:54 +0530

    basic oops project developed

---
+++ OOPS_Proj.py
@@ -0,0 +1,29 @@
+class chatbook:
+    def __init__(self):
+        self.username = ''
+        self.password = ''
+        self.loggedin = False
+        self.menu()
+    
+
+    def menu(self):
+        user_input = input("""Welcome to the chatbook !! Can you tell me how would you like to proceed?
+                           1. Press 1 to Signup
+                           2. Press 2 to Signin
+                           3. Press 3 to write a post
+                           4. Press 4 to message a Friend
+                           5. Press any other key to exit""")
+        
+        if user_input == "1":
+            pass
+        elif user_input=="2":
+            pass
+        elif user_input == "3":
+            pass
+        elif user_input == "4":
+            pass
+        else:
+            exit()
+
+obj1 = chatbook()
+
