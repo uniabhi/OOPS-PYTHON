5b16090724a88ccdf691dc83924b27a34932d0c7
uniabhi <uniabhi5684@gmail.com>
2026-01-19 14:38:29 +0530

    getter, setter and statci method added

--- oops1.py
+++ oops1.py
@@ -1,23 +1,33 @@
 class employee:
+    
     def __init__(self):
+        # print(id(self))
         print("Moving to data/attribute definition")
+        self.name = "Default User"
         self.id = 123
         self.salary = 23000
         self.designation = "MLE"
-        print("Definition of data/attribute completed")
+        # print("Definition of data/attribute completed")
 
 #Create object/ instance of class
 
-    def travel(self, destination):
+    def travel(self):
         print("Manually calling this travel method")
-        print(f"Employee is now travelling to {destination}")
+        print(f"Employee is now travelling to delhi")
 
 
 abhi = employee()
+print(id(abhi))
+
+
+kumar = employee()
 #printing a attribute/variable
 # print(abhi.id)
 
 #calling a method functionality
-abhi.travel("Paris")
+# abhi.travel("Paris")
+print(id(kumar))
 
-print(type(abhi))
+# print(type(abhi))
+abhi.name = "Singh"
+print(abhi.name)
--- rough.py
+++ rough.py
@@ -1,13 +1,42 @@
-# from OOPS_Proj import chatbook
+from OOPS_Proj import chatbook
 
 lst = [1,2,3]
 my_str = "mlps playlist"
 my_int = 123
 
-print(type(lst))
-print(type(my_str))
-print(type(my_int))
+# print(type(lst))
+# print(type(my_str))
+# print(type(my_int))
 
-print(lst.index(1))
+# print(lst.index(1))
 
-# user1 = chatbook()
+from OOPS_Proj import chatbook
+user1 = chatbook()
+print(user1.id)
+
+
+lst1 = [1,2,3]
+
+# print(len(lst1))
+
+# user1.signup()
+# print(user1._chatbook__name)
+
+# print(user1.get_name())
+# user1.set_name("Harish")
+# print(user1.get_name())
+
+
+# Using static method directly from class rather than object
+
+# Using static method directly from class rather than obj
+chatbook.set_id(10)
+
+user2 = chatbook()
+print(user2.id)
+
+user3 = chatbook()
+print(user3.id)
+
+user4 = chatbook()
+print(user4.id)
